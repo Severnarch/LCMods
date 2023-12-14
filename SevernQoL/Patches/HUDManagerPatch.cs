570bb4f4f64d27bc592ffd8d4aee2e58bfccfb40
Severnarch <86313252+Severnarch@users.noreply.github.com>
2023-12-14 20:14:22 +0000

    Cleanup

--- SevernQoL/Patches/HUDManagerPatch.cs
+++ SevernQoL/Patches/HUDManagerPatch.cs
@@ -1,7 +1,4 @@
-﻿using GameNetcodeStuff;
-using HarmonyLib;
-using System;
-using TMPro;
+﻿using HarmonyLib;
 using UnityEngine;
 
 namespace SevernQoL.Patches
@@ -9,17 +6,17 @@ namespace SevernQoL.Patches
     [HarmonyPatch(typeof(HUDManager))]
     internal class HUDManagerPatch
     {
-        [HarmonyPatch(typeof(HUDManager), "Update")]
+        [HarmonyPatch("Update")]
         [HarmonyPostfix]
-        private static void UpdatePatch(ref TextMeshProUGUI ___weightCounter, ref Animator ___weightCounterAnimator)
+        private static void UpdatePatch()
         {
             if (Configuration.KiloWeightEnabled.Value == true)
             {
                 if (!GameNetworkManager.Instance.localPlayerController.isPlayerDead)
                 {
                     float num2 = Mathf.RoundToInt(Mathf.Clamp(GameNetworkManager.Instance.localPlayerController.carryWeight - 1f, 0f, 100f) * 105f / 2.205f);
-                    ___weightCounter.text = $"{num2} kg";
-                    ___weightCounterAnimator.SetFloat("weight", num2 / 130f);
+                    HUDManager.Instance.weightCounter.text = $"{num2} kg";
+                    HUDManager.Instance.weightCounterAnimator.SetFloat("weight", num2 / 130f);
                 }
             }
         }
